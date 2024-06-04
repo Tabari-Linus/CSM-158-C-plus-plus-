3322f1ae3e8c469783ca3b42ab1f2bd05044cbfe
gf1 <gf1@gmail.com>
2024-06-04 09:33:01 +0000

    looping statements done

---
+++ 4thJune/loops/dowhileloop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int count = 11;
+
+do
+{
+   cout<<"Number: "<<count<<endl;
+   count++;
+}while (count<=10);
+
+return 0;
+}
---
+++ 4thJune/loops/whileloops.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int count = 0;
+while (count<=10)
+{
+   cout<<"Number: "<<count<<endl;
+   count++;
+}
+
+return 0;
+}
